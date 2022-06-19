a95a6473addf2c711817ad198e800ad0f2c27364
aniqfakhrul <aniqfakhrul@mcoba.org>
2022-06-19 12:13:36 -0400

    Updated

---
+++ utils/completer.py
@@ -0,0 +1,63 @@
+import os
+import re
+import readline
+
+COMMANDS = ['Get-Domain', 'Get-DomainUser', 'Get-DomainComputer', 'Get-DomainGroup', 'Get-Forest','Get-DomainGPO','Get-DomainController','-Identity']
+RE_SPACE = re.compile('.*\s+$', re.M)
+
+class Completer(object):
+
+    def _listdir(self, root):
+        "List directory 'root' appending the path separator to subdirs."
+        res = []
+        for name in os.listdir(root):
+            path = os.path.join(root, name)
+            if os.path.isdir(path):
+                name += os.sep
+            res.append(name)
+        return res
+
+    def _complete_path(self, path=None):
+        "Perform completion of filesystem path."
+        if not path:
+            return self._listdir('.')
+        dirname, rest = os.path.split(path)
+        tmp = dirname if dirname else '.'
+        res = [os.path.join(dirname, p)
+                for p in self._listdir(tmp) if p.startswith(rest)]
+        # more than one match, or single match which does not exist (typo)
+        if len(res) > 1 or not os.path.exists(path):
+            return res
+        # resolved to a single directory, so return list of files below it
+        if os.path.isdir(path):
+            return [os.path.join(path, p) for p in self._listdir(path)]
+        # exact file match terminates this completion
+        return [path + ' ']
+
+    def complete_extra(self, args):
+        "Completions for the 'extra' command."
+        if not args:
+            return self._complete_path('.')
+        # treat the last arg as a path and complete it
+        return self._complete_path(args[-1])
+
+    def complete(self, text, state):
+        "Generic readline completion entry point."
+        buffer = readline.get_line_buffer()
+        line = readline.get_line_buffer().split()
+        # show all commands
+        if not line:
+            return [c + ' ' for c in COMMANDS][state]
+        # account for last argument ending in a space
+        if RE_SPACE.match(buffer):
+            line.append('')
+        # resolve command to the implementation function
+        cmd = line[0].strip()
+        if cmd in COMMANDS:
+            impl = getattr(self, 'complete_%s' % cmd)
+            args = line[1:]
+            if args:
+                return (impl(args) + [None])[state]
+            return [cmd + ' '][state]
+        results = [c + ' ' for c in COMMANDS if c.startswith(cmd)] + [None]
+        return results[state]
---
+++ utils/pywerview.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+class PywerView:
+    
+    def __init__(self,ldap_session,root_dn):
+        self.ldap_session = ldap_session
+        self.root_dn = root_dn
+
+    def get_domainuser(self, args, properties='*'):
+        if args.preauthnotrequired:
+            ldap_filter = f'(&(samAccountType=805306368)(userAccountControl:1.2.840.113556.1.4.803:=4194304)(sAMAccountName={args.identity}))'
+        elif args.spn:
+            ldap_filter = f'(&(samAccountType=805306368)(servicePrincipalName=*)(sAMAccountName={args.identity}))'
+        else:
+            ldap_filter = f'(&(samAccountType=805306368)(sAMAccountName={args.identity}))'
+
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+
+    def get_domaincontroller(self, args, properties='*'):
+        ldap_filter = f'(userAccountControl:1.2.840.113556.1.4.803:=8192)'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+
+    def get_domaincomputer(self, args, properties='*'):
+        if args.unconstrained:
+            ldap_filter = f'(&(samAccountType=805306369)(userAccountControl:1.2.840.113556.1.4.803:=524288)(sAMAccountName={args.identity}))'
+        elif args.trustedtoauth:
+            ldap_filter = f'(&(samAccountType=805306369)(msds-allowedtodelegateto=*)(sAMAccountName={args.identity}))'
+        else:
+            ldap_filter = f'(&(samAccountType=805306369)(sAMAccountName={args.identity}))'
+        
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+        #for entry in self.ldap_session.entries:
+        #    print(entry.entry_to_ldif())
+    
+    def get_domaingroup(self, args, properties='*'):
+        ldap_filter = f'(&(objectCategory=group)(name={args.identity}))'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+    
+    def get_domaingpo(self, args, properties='*'):
+        ldap_filter = f'(&(objectCategory=groupPolicyContainer)(cn={args.identity}))'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+    
+    def get_domaintrust(self, args, properties='*'):
+        ldap_filter = f'(objectClass=trustedDomain)'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+    
+    def get_domain(self, args, properties='*'):
+        ldap_filter = f'(objectClass=domain)'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=properties)
+        return self.ldap_session.entries
+    
+    def add_domaingroupmember(self, args):
+        ldap_filter = f'(&(objectCategory=group)(name={args.identity}))'
+        self.ldap_session.search(self.root_dn,ldap_filter,attributes=['distinguishedname'])
+        identity_dn = self.ldap_session.entries[0].entry_dn
+
+        self.ldap_session.modify(identity_dn, {'member':[ldap3.MODIFY, [args.members]]})
