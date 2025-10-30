1e709d4256768edfbc96c240f85622af82491f88
Your Name <you@example.com>
2025-10-30 12:41:45 +0000

    updated

--- powerview/utils/connections.py
+++ powerview/utils/connections.py
@@ -639,7 +639,7 @@ class CONNECTION:
 		self._current_domain = None
 		self.username = args.username
 		self.password = args.password
-		self.domain = args.domain or dn2domain(self.alt_server_info["defaultNamingContext"][0]) if self.alt_server_info and hasattr(self.alt_server_info, "defaultNamingContext") and len(self.alt_server_info["defaultNamingContext"]) > 0 else None
+		self.domain = args.domain
 		self.lmhash = args.lmhash
 		self.nthash = args.nthash
 		self.use_kerberos = args.use_kerberos
@@ -1234,12 +1234,12 @@ class CONNECTION:
 		if self.use_kerberos:
 			try:
 				if ldap_address and is_ipaddress(ldap_address):
-					target = self.alt_server_info["dnsHostName"][0] if self.alt_server_info and hasattr(self.alt_server_info,"dnsHostName") and len(self.alt_server_info["dnsHostName"]) > 0 else None
+					target = self.alt_server_info["dnsHostName"][0] if self.alt_server_info and hasattr(self.alt_server_info, "dnsHostName") and len(self.alt_server_info["dnsHostName"]) > 0 else None
 					if not target:
 						target = get_machine_name(ldap_address)
 					self.kdcHost = target
 				elif self.ldap_address is not None and is_ipaddress(self.ldap_address):
-					target = self.alt_server_info["dnsHostName"][0] if self.alt_server_info and hasattr(self.alt_server_info,"dnsHostName") and len(self.alt_server_info["dnsHostName"]) > 0 else None
+					target = self.alt_server_info["dnsHostName"][0] if self.alt_server_info and hasattr(self.alt_server_info, "dnsHostName") and len(self.alt_server_info["dnsHostName"]) > 0 else None
 					if not target:
 						target = get_machine_name(self.ldap_address)
 					self.kdcHost = target
